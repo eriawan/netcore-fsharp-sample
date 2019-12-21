47b4c9dd6441a17e28de07830028a2fb52e92ce0
Eriawan Kusumawardhono <eriawan@live.com>
2019-12-21 18:07:31 +0700

    update .NET Core 3.1 sample for WinForms with FrameworkReference

--- src/netcore3.1/FSWindowsDesktop/Program.fs
+++ src/netcore3.1/FSWindowsDesktop/Program.fs
@@ -1,8 +1,22 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open System.Windows.Forms
+open System.Drawing
+
+let InitializeComponent (frmInitialized : Form) =
+    let currentOSVersion = Environment.OSVersion.Version.Major.ToString() + "." + Environment.Version.Minor.ToString() + "." + Environment.OSVersion.Version.Build.ToString()
+    let mutable btnHelloWinForm = new Button()
+    btnHelloWinForm.Text <- "Hello F# WinForms"
+    btnHelloWinForm.Location <- new Point(40, 100)
+    btnHelloWinForm.Click.Add(fun evtArgs -> MessageBox.Show("Runs on Windows version "+ currentOSVersion) |> ignore)
+    frmInitialized.Controls.Add(btnHelloWinForm)
+    ()
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let mainForm = new Form()
+    InitializeComponent(mainForm)
+    Application.Run(mainForm)
     0 // return an integer exit code
