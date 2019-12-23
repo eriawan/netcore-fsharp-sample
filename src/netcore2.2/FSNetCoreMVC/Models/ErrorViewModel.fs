523fca2252fe23e832868c0641cd5de0f6e823c4
eriawan <eriawan@live.com>
2019-12-23 15:55:29 +0700

    upload sample solution and F# MVC project in .NET Core SDK 2.2.207

---
+++ src/netcore2.2/FSNetCoreMVC/Models/ErrorViewModel.fs
@@ -0,0 +1,8 @@
+namespace FSNetCoreMVC
+
+open System
+
+type ErrorViewModel private () =
+    member val RequestId : string = null with get, set
+
+    member val ShowRequestId : bool = true with get, set
